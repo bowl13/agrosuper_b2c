4895943944e250479fee845bd552a1e479723e07
bowl13 <mauro@bowl.cl>
2013-06-20 12:03:37 -0400

    update_video

---
+++ cont_v.php
@@ -0,0 +1,13 @@
+<div class="load"></div>
+<div id="wrap">
+	<?php include("menu.php"); ?>
+	<div class="container-fluid">
+		
+	</div>
+	<div id="push"></div>
+</div>
+<footer class="footer-fix container-fluid text-center">
+	<div class="row-fluid">
+		<div class="span12">&copy; 2013 Agrosuper. All rights reserved. | Camino la Estrella Nº 401, of. 7, Sector Punta de Cortes, | Rancagua. Región del Libertador General Bernardo O'Higgins, Chile.</div>
+	</div>
+</footer>
